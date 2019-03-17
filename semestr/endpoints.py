ccf9e3b31674fa68a079b396c5c1611e2881194b
Daniel Gal <daniel.gal@sch.bme.hu>
2019-03-17 14:56:59 +0100

    Nested routers

--- semestr/endpoints.py
+++ semestr/endpoints.py
@@ -1,15 +1,19 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework_nested import routers
 
 from . import api
 
-router = routers.DefaultRouter()
-router.register(r'semester', api.SemesterAPI, 'semester')
-router.register(r'class', api.ClassAPI)
-router.register(r'requirement', api.RequirementAPI)
+router = routers.SimpleRouter()
+router.register(r'semesters', api.SemesterAPI, base_name='semesters')
+semesters_router = routers.NestedSimpleRouter(router, r'semesters', lookup='semester')
+semesters_router.register(r'classes', api.ClassAPI, base_name='semester-classes')
+classes_router = routers.NestedSimpleRouter(semesters_router, r'classes', lookup='class')
+classes_router.register(r'requirements', api.RequirementAPI, base_name='class-requirements')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(semesters_router.urls)),
+    path('', include(classes_router.urls)),
     path('auth/register/', api.RegistrationAPI.as_view()),
     path('auth/login/', api.LoginAPI.as_view()),
     path('auth/user/', api.UserAPI.as_view())
