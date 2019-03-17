33256b0e7dcfc49578d634be7765b54aef9664b7
Daniel Gal <daniel.gal@sch.bme.hu>
2019-03-17 16:30:29 +0100

    Preparing test framework for endpoints

--- semestr/tests.py
+++ semestr/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Semester
+
+class SemesterTests(APITestCase):
+    pass
