f053fe650c9376ce09265a6a1929808897c3b48a
Daniel Gal <daniel.gal@sch.bme.hu>
2019-04-01 03:01:36 +0200

    New UserSerializer

--- semestr/api.py
+++ semestr/api.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets, permissions, generics, mixins
+from rest_framework import viewsets, permissions, generics, mixins, status
 from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 from . import models
 from . import serializers
@@ -47,3 +49,23 @@ class UserAPI(generics.RetrieveAPIView):
 class RegisterAPI(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny, ]
     serializer_class = serializers.RegisterUserSerializer
+
+
+class LoginAPI(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user': serializers.UserSerializer(user).data
+        })
+
+
+class LogoutAPI(generics.GenericAPIView):
+    def get(self, request, format=None):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
